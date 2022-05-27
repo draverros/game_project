74d64e00283c8180ad7d16d75c40bc0c67d82dd6
draverros <105558472+draverros@users.noreply.github.com>
2022-05-27 17:20:57 +0100

    Update player.py
    Added charisma stat

--- player.py
+++ player.py
@@ -14,12 +14,14 @@ class Player:
         self.dexterity_roll = 0
         self.intelligence_roll = 0
         self.constitution_roll = 0
+        self.charisma_roll = 0
 
         # Race Stats
         self.strength_race = 0
         self.dexterity_race = 0
         self.intelligence_race = 0
         self.constitution_race = 0
+        self.charisma_race = 0
         self.hp_race = 0
 
         # Class Stats
@@ -27,6 +29,7 @@ class Player:
         self.dexterity_profession = 0
         self.intelligence_profession = 0
         self.constitution_profession = 0
+        self.charsma_profession = 0
         self.hp_profession = 0
         self.mp_profession = 0
 
@@ -38,6 +41,7 @@ class Player:
         self.dexterity = 0
         self.intelligence = 0
         self.constitution = 0
+        self.charisma = 0
         self.hp = 0
         self.mp = 0
 
