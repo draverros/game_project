cc4c4635e1d2ef9b1aa4cd4b3e044dab9e88e31f
draverros <105558472+draverros@users.noreply.github.com>
2022-05-27 15:44:18 +0100

    Update game.py

--- game.py
+++ game.py
@@ -322,7 +322,7 @@ def setup_game():
     print(f"Welcome, {player_name}. You are a {player_race}, a {player_profession}.")
 
     while not myPlayer.game_over:
-        choice = check_menu_range(city_nev.neverwinter_menu)
+        choice = check_menu_range("> ", city_nev.neverwinter_menu)
 
         def show_inventory(inventory_list):
             if len(inventory_list) < 1:
